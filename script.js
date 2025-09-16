1734323f5586508ae9b2076cd75f52a254827c99
Everjust <company@everjust.org>
2025-09-16 18:01:20 -0500

    Add security headers and HTTPS redirect for secure connection

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // NEXUS Website JavaScript
 
+// Force HTTPS redirect
+(function() {
+    if (location.protocol !== 'https:' && location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
+        location.replace('https:' + window.location.href.substring(window.location.protocol.length));
+    }
+})();
+
 // DOM Elements
 const navToggle = document.getElementById('nav-toggle');
 const navMenu = document.getElementById('nav-menu');
